dab43908c7a193aac0dd7c024ae4225596241fc4
jaivo45 <jaivo45@gmail.com>
2017-03-10 01:54:47 -0600

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
      mysqli_select_db($connection, $db_name) or die("Error al seleccionar la base de datos:".mysqli_error());
     @mysqli_query("SET NAMES 'utf8'");
 
-    $sql_query = "SELECT * FROM contactos";
+    $sql_query = "SELECT * FROM productos";
     $result = mysqli_query($connection, $sql_query);
     $rows = array();
 while($r = mysqli_fetch_assoc($result)) {
